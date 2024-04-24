b58a398605a242cff2b34a0cec1793b0798bb555
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-04-24 13:53:28 +0530

    AppWrite setup, signin, signup

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,17 +1,38 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: "", password: "" });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
-    console.log(form);
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
+  const submit = async () => {
+    if (!form.email || !form.password)
+      Alert.alert("Error", "Please fill in all the fields");
+
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const res = getCurrentUser();
+
+      setUser(res);
+      setIsLoggedIn(true);
+
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,17 +1,36 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
-    console.log(form);
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
+  const submit = async () => {
+    if (!form.email || !form.username || !form.password)
+      Alert.alert("Error", "Please fill in all the fields");
+
+    setIsSubmitting(true);
+
+    try {
+      const res = await createUser(form.email, form.password, form.username);
+
+      setUser(res);
+      setIsLoggedIn(true);
+
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
